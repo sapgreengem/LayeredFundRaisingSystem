d650180107903d91112139b4d9a5942c8b8655d0
Sapgreen Gem <sapgreengem@gmail.com>
2017-10-12 02:06:45 +0600

    layout category list added

---
+++ layeredfundraisersystem/Models/LoadAllCategory.cs
@@ -0,0 +1,27 @@
+﻿using FundRaiserSystemData;
+using FundRaiserSystemEntity;
+using FundRaiserSystemService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace layeredFundRaiserSystem.Models
+{
+    public class LoadAllCategory
+    {
+        public IEnumerable<PostingCategory> getCategory()
+        {
+            IPostingCategoryService catService = ServiceFactory.GetPostingCategoryService();
+            IEnumerable<PostingCategory> loadCategories = catService.GetAll();
+            return loadCategories;
+        }
+
+        public IEnumerable<FundRequestPost> GetPostsById(int Category)
+        {
+            IFundRequestPostService service = ServiceFactory.GetFundRequestPostService();
+            IEnumerable<FundRequestPost> post = service.GetAll().Where(b => b.CategoryId == Category).Where(a => a.PostStatus == "Active");
+            return post;
+        }
+    }
+}
