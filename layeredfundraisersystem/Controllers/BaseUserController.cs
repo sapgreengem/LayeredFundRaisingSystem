dbba74a99b5bd97222d12ff2efc3bb9b93fe210c
Sapgreen Gem <sapgreengem@hotmail.com>
2017-11-18 08:12:19 +0600

    tried to do something with base controller

--- layeredfundraisersystem/Controllers/BaseUserController.cs
+++ layeredfundraisersystem/Controllers/BaseUserController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FundRaiserSystemData;
+using FundRaiserSystemEntity;
+using FundRaiserSystemService;
 
 namespace layeredFundRaiserSystem.Controllers
 {
@@ -19,12 +22,24 @@ namespace layeredFundRaiserSystem.Controllers
             {
                 Response.Redirect("/Login");
             }
+            //else if (Session["Login"] != null)
+            //{
+            //    int check = 0;
+            //    IUserInformationService service = ServiceFactory.GetUserInformationService();
+            //    check = service.GetAll().Where(a => a.UserId == Convert.ToInt32(Session["Login"])).Count();
+            //    if (check <= 0)
+            //    {
+            //        Response.Redirect("/FillAllInfo/Index");
+            //    }
+            //}
 
             if (Session["UserInformationId"] != null)
             {
                 ShowUserName name = new ShowUserName();
                 ViewBag.LoginName = name.UserName(Convert.ToInt32(Session["UserInformationId"]));
             }
+
+
             
         }
     }
